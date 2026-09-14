03489b87db886e58d3a2624c28d9268d6ce86e03
thutazawhan <thutazawjohan.email@example.com>
2026-09-14 15:59:57 +0700

    test two

--- lab0/nlm.java
+++ lab0/nlm.java
@@ -3,5 +3,6 @@ package lab0;
 public class nlm {
     public static void main(String[] args) {
         System.out.println("Nga Loe Ma tr");
+        System.out.println("Enter your name");
     }
 }
